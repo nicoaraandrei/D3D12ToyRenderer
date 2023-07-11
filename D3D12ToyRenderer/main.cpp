251067c498e9a9ff7bf6d04744c8e1c34e6cf8af
nicoaraandrei <AndreiN95@gmail.com>
2023-07-11 13:57:42 +0300

    D3D Device + Command Queue initialization

--- D3D12ToyRenderer/main.cpp
+++ D3D12ToyRenderer/main.cpp
@@ -4,10 +4,16 @@
 #include <Support/ComPointer.h>
 
 #include <DxDebug/DXDebugLayer.h>
+#include <D3D/DxContext.h>
 
 int main()
 {
 	DXDebugLayer::Get().Init();
+
+	if (DXContext::Get().Init())
+	{
+		DXContext::Get().Shutdown();
+	}
 
 	DXDebugLayer::Get().Shutdown();
 }
